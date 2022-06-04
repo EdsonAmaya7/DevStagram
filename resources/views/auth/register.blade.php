d95c375b2cbc88c6e92f0ea78beb2d46a3de39d5
Edson Amaya <edson_amaya99@live.com>
2022-06-04 11:52:09 -0500

    first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app ')
+
+
+
+@section('titulo')
+
+Registrate en DevsTagram
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <title>Devstagram - @yield('titulo')</title>
+
+</head>
+
+
+<body class="bg-gray-100">
+
+
+    <header class="p-5 border-b bg-white shadow">
+
+    <div class="container mx-auto flex justify-between items-center">
+
+        <h1 class="text-3xl font-black">DevStagram</h1>
+
+        <nav class="flex gap-2 items-center">
+        <a href="#" class="font-bold uppercase text-gray-600 text-sm">Login</a>
+        <a href="#" class="font-bold uppercase text-gray-600 text-sm">Crear Cuenta</a>
+        </nav>
+
+    </div>
+
+    </header>
+
+
+    <main class="container mx-auto mt-10">
+
+        <h2 class="font-black text-center text-3xl mt-10">@yield('titulo')</h2>
+        @yield('contenido')
+    </main>
+
+    <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+
+        DevStagram -Todos los derechos reservados
+            {{ now()->year  }}
+
+    </footer>
+</body>
+
+</html>
